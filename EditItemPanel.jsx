2413b898fd9916352622e90d213a67b7e8869285
yourhouseholdhero <yourhouseholdhero@gmail.com>
2025-07-20 02:33:55 -0400

    Update EditItemPanel.jsx

--- EditItemPanel.jsx
+++ EditItemPanel.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
 
+export default function EditItemPanel({ item, onUpdate }) {
+  return (
+    <div className="border p-3 rounded bg-slate-50">
+      <h3 className="font-semibold">Edit Item</h3>
+      <input type="text" value={item.title} onChange={e => onUpdate('title', e.target.value)} />
+      <textarea value={item.description} onChange={e => onUpdate('description', e.target.value)} />
+      <input type="number" value={item.price} onChange={e => onUpdate('price', e.target.value)} />
+    </div>
+  );
+}
