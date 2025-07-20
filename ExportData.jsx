e903f9aa6e0065d76b58b2ff5140a8294347898c
yourhouseholdhero <yourhouseholdhero@gmail.com>
2025-07-20 02:34:18 -0400

    Update ExportData.jsx

--- ExportData.jsx
+++ ExportData.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
 
+export default function ExportData({ data }) {
+  const exportCSV = () => {
+    const csv = data.map(d => Object.values(d).join(',')).join('\n');
+    const blob = new Blob([csv], { type: 'text/csv' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'inventory.csv';
+    a.click();
+  };
+
+  return <button onClick={exportCSV} className="bg-gray-700 text-white py-2 px-4 rounded">Export CSV</button>;
+}
