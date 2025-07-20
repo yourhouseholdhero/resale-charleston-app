a2eca8ce1939206800fd7feb9b7db8435ddae33c
yourhouseholdhero <yourhouseholdhero@gmail.com>
2025-07-20 02:36:47 -0400

    Update PaymentOptions.jsx

--- PaymentOptions.jsx
+++ PaymentOptions.jsx
@@ -1,1 +1,14 @@
+import React from 'react';
 
+export default function PaymentOptions({ paymentType, setPaymentType }) {
+  return (
+    <div className="mb-4">
+      <label className="block font-semibold">Payment Method:</label>
+      <select value={paymentType} onChange={e => setPaymentType(e.target.value)}>
+        <option>Cash</option>
+        <option>Card</option>
+        <option>Venmo</option>
+      </select>
+    </div>
+  );
+}
