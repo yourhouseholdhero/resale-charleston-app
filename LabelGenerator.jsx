276ad28f49af5c29e83dcb06463c7d9cb95e77f2
yourhouseholdhero <yourhouseholdhero@gmail.com>
2025-07-20 02:34:40 -0400

    Update LabelGenerator.jsx

--- LabelGenerator.jsx
+++ LabelGenerator.jsx
@@ -1,1 +1,11 @@
+import React from 'react';
+import QRCode from 'qrcode.react';
 
+export default function LabelGenerator({ link }) {
+  return (
+    <div className="text-center mt-4">
+      <QRCode value={link} />
+      <p className="text-sm">{link}</p>
+    </div>
+  );
+}
