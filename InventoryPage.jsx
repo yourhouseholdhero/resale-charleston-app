25c08798af111160321b65eaea8cbe7ee50d2563
yourhouseholdhero <yourhouseholdhero@gmail.com>
2025-07-28 22:45:34 -0400

    Update InventoryPage.jsx

--- InventoryPage.jsx
+++ InventoryPage.jsx
@@ -23,6 +23,7 @@ export default function InventoryPage() {
         <table className="w-full border">
           <thead>
             <tr className="bg-gray-100">
+              <th className="p-2 border">Image</th>
               <th className="p-2 border">Name</th>
               <th className="p-2 border">Owner</th>
               <th className="p-2 border">Price</th>
@@ -33,6 +34,13 @@ export default function InventoryPage() {
           <tbody>
             {items.map(item => (
               <tr key={item.id} className="border-t">
+                <td className="p-2 border">
+                  {item.images?.[0] ? (
+                    <img src={item.images[0]} alt="Item" className="w-20 h-20 object-cover" />
+                  ) : (
+                    <span className="text-gray-400">No image</span>
+                  )}
+                </td>
                 <td className="p-2 border">{item.name}</td>
                 <td className="p-2 border">{item.owner}</td>
                 <td className="p-2 border">${item.price}</td>
